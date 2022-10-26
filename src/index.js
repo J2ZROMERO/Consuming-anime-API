02657cf702c6e549129709c68c818c6558b521ba
Jhosep <jose-zromero@hotmail.com>
2022-10-25 21:49:27 -0500

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 import './style.css';
 import pokeicon from './img/pokeicon.png';
 
-import RequestApi from './modules/requestApi.js';
-
-const api = new RequestApi();
-
 document.querySelector('.logo-image').src = pokeicon;
-
--- src/modules/requestApi.js
+++ src/modules/requestApi.js
@@ -1,17 +1,11 @@
 /* eslint no-iterator: "error" */
-import lielement from './eleLiPok.js';
 
 export default class Requestapi {
- 
-
-   
-
     getsPokemons = async () => {
       const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0');
       const data = await response.json();
       return data;
     }
-
 
   contadorElementos = async () => {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0');
